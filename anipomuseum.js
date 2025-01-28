b334b3e03cba965805e68c86e232ea630e18414b
Sola <1216sola@gmail.com>
2025-01-28 14:26:09 +0900

    commit

--- anipomuseum.js
+++ anipomuseum.js
@@ -2,18 +2,18 @@ const museumGallery = document.getElementById('museum-gallery');
 
 // デフォルト画像リスト
 const defaultImages = [
-    "1.png",
-    "2.png",
-    "3.png",
-    "4.png",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
     "5.png",
     "6.png",
     "7.png",
     "8.png",
-    "9.png",
-    "10.png",
-    "11.png",
-    "12.png",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
 ];
 
 // IndexedDBまたはlocalStorageから画像を取得
